2224ffbf45d178d2c0c11ab5a01b3b63b582e2c6
iamalmiir <iamalmiir@gmail.com>
2022-12-08 01:23:28 -0500

    Initial code structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,11 @@
-import styles from '../styles/Home.module.css'
-import axios from 'axios'
+import AppBar from '../common/components/AppBar/AppBar.jsx'
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1>Home Page</h1>
+    <div>
+      <AppBar />
     </div>
   )
-}
-
-export const getStaticProps = async (context) => {
-  const data = await axios.get('http://localhost:3000/api/hello/')
-  console.log(data)
-  return {
-    props: {},
-  }
 }
 
 export default Home
