7902b3c4727ec79af5b0023c22604f851f00fe05
Almir Redzematovic <almir@iamalmir.tech>
2023-01-30 01:39:27 -0500

    Added AOS for animations

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,13 @@ import '@styles/globals.css'
 import '@fontsource/playfair-display'
 import '@fontsource/montserrat-alternates'
 import { Fragment } from 'react'
+import dynamic from 'next/dynamic'
 import { Analytics } from '@vercel/analytics/react'
+const AOS = dynamic(() => import('@components/CustomAOS'))
 
 const MyApp = ({ Component, pageProps }) => (
   <Fragment>
+    <AOS />
     <Component {...pageProps} />
     <Analytics />
   </Fragment>
