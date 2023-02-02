35083b086df7c122a1adee20fab1c8c1b7a05342
Almir Redzematovic <almir@iamalmir.tech>
2023-02-02 14:29:35 -0500

    Added hover affect for footer links

--- src/common/components/Footer.jsx
+++ src/common/components/Footer.jsx
@@ -6,7 +6,7 @@ import { OuterContainer } from '@components/Container'
 export const Footer = () => {
   return (
     <OuterContainer data-aos='fade-up'>
-      <div className='mt-16 border-t p-8 border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24'>
+      <div className='mt-16 border-t p-6 border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24'>
         <div className='flex space-x-6 md:order-2'>
           {socialLinks.map((item) => (
             <Link
@@ -14,7 +14,11 @@ export const Footer = () => {
               href={item.href}
               target='_blank'
               rel='noopener noreferrer'
-              className='text-gray-500 hover:text-gray-400'
+              className={`${
+                item.name === 'GitHub'
+                  ? 'hover:bg-black'
+                  : 'hover:bg-indigo-700'
+              } text-gray-500 p-3 rounded-full hover:text-gray-300`}
             >
               <span className='sr-only'>{item.name}</span>
               <item.icon className='h-6 w-6' aria-hidden='true' />
