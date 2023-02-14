c2b4663ae1954e87f1333b0ecab7b4cdf4cca25f
Peter V <98245483+p5quared@users.noreply.github.com>
2023-02-14 07:44:32 -0500

    Renamed contacts -> join. Started working on index.jsx

---
+++ src/pages/join/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Join = () => {
+  return <div className='pt-6'>
+            <div className='flex-col text-slate-200 items-center justify-center'>
+              <h1>Join</h1>
+              <p>There are a couple options for you to join our club.
+                First, you may fill out the Google form below.
+                You may also join our discord and talk to one of our officers.
+                Or, you can feel free to attend any of our meetings in person and talk to us there.
+              </p>
+            </div>
+         </div>
+}
+
+export default Join
